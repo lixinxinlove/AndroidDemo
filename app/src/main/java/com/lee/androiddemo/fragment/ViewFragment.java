67ed5aeab423e76ce2a448a20ca8fc3502750e16
lixinxin <895330766@qq.com>
2017-06-22 11:15:11 +0800

    添加 属性动画

--- app/src/main/java/com/lee/androiddemo/fragment/ViewFragment.java
+++ app/src/main/java/com/lee/androiddemo/fragment/ViewFragment.java
@@ -1,10 +1,17 @@
 package com.lee.androiddemo.fragment;
 
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import com.lee.androiddemo.R;
 
 public class ViewFragment extends BaseFragment {
 
+
+    ImageView ivLogo;
 
     @Override
     protected int getContextView() {
@@ -13,12 +20,34 @@ public class ViewFragment extends BaseFragment {
 
     @Override
     protected void findView() {
-
+        ivLogo = (ImageView) rootView.findViewById(R.id.iv_logo);
     }
 
     @Override
     protected void initData() {
 
+        ValueAnimator anim = ValueAnimator.ofFloat(0f, 3f, 2f, 1f, -5f);
+        anim.setDuration(5000);
+        anim.setRepeatCount(10000);
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentValue = (float) animation.getAnimatedValue();
+
+                ViewGroup.LayoutParams param = ivLogo.getLayoutParams();
+                param.height = (int) (ivLogo.getHeight() + currentValue);
+                param.width = (int) (ivLogo.getWidth() + currentValue);
+                ivLogo.setLayoutParams(param);
+            }
+        });
+        anim.setStartDelay(1000);
+
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(ivLogo, "alpha", 1f, 0f, 1f);
+        animator.setDuration(5000);
+        animator.setRepeatCount(1000);
+        animator.setStartDelay(1000);
     }
 
     @Override
