acbda76e6b5d7a45e5935e0c9c902c02791d0fad
lixinxin <895330766@qq.com>
2017-07-17 18:27:24 +0800

    测试

--- app/src/main/java/com/lee/androiddemo/activity/DialogFragmentActivity.java
+++ app/src/main/java/com/lee/androiddemo/activity/DialogFragmentActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.lee.androiddemo.R;
 import com.lee.androiddemo.dialog.LeeDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -19,15 +28,28 @@ public class DialogFragmentActivity extends BaseActivity {
     @BindView(R.id.btn_menu)
     Button btnMenu;
 
+    @BindView(R.id.list_view_menu)
+    ListView listView;
+
+
+    List<String> mData;
+
+
     PopupMenu popupMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mData = new ArrayList<>();
+
+        for (int i = 0; i < 15; i++) {
+            mData.add("lee" + i);
+        }
+
+
         popupMenu = new PopupMenu(this, btnMenu);
         popupMenu.inflate(R.menu.menu2);
-
-
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -35,6 +57,37 @@ public class DialogFragmentActivity extends BaseActivity {
                 return false;
             }
         });
+
+
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return mData.size();
+            }
+
+            @Override
+            public String getItem(int position) {
+                return mData.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                convertView = View.inflate(DialogFragmentActivity.this, R.layout.item_view, null);
+
+                TextView textView = (TextView) convertView.findViewById(R.id.tv_top);
+                textView.setText(mData.get(position));
+                return convertView;
+            }
+        });
+
+
+
 
 
     }
@@ -55,7 +108,15 @@ public class DialogFragmentActivity extends BaseActivity {
         });
 
 
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                popupMenu = new PopupMenu(DialogFragmentActivity.this, view);
+                popupMenu.inflate(R.menu.menu2);
+                popupMenu.setGravity(Gravity.RIGHT);
+                popupMenu.show();
+            }
+        });
 
     }
 
